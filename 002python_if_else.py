ed8a9062a0a97b43ca751cdd3a85efd08a234a5b
Semihcan Doken <semihcandoken@Semihcans-MacBook-Pro.local>
2017-03-31 16:17:15 -0400

    First Solution Attempt

---
+++ 002python_if_else.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__' :
+    n = int(input())
+    if n%2 == 1:
+        print('Weird')
+    elif (n<=5 and n>=2):
+        print("Not Weird")
+    elif (n>=6 and n<=20):
+        print("Weird")
