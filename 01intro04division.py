64057c55cd177633f9e08020db4ae0ca2483f7da
Semihcan Doken <semihcandoken@Semihcans-MacBook-Pro.local>
2017-04-04 09:33:50 -0400

    division problem solution

---
+++ 01intro04division.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b) # in python3, // is integer division
+    print(a/b)  # in python3, / assumes floating division
